abd662ca120b53c5d26a51a07c51d3d543035224
JesusDHernandezR <jesusdavidhernandez@unicesar.edu.co>
2021-09-07 10:23:03 -0500

    commit componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RestaComponent } from './resta/resta.component';
+import { SumaComponent } from './suma/suma.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'add', component:SumaComponent},
+  {path:'resta', component:RestaComponent},
+  {path:'home', component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OperacionComponent } from './operacion/operacion.component';
+import { AprendeComponent } from './aprende/aprende.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { RestaComponent } from './resta/resta.component';
+import { SumaComponent } from './suma/suma.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OperacionComponent,
+    AprendeComponent,
+    MenuComponent,
+    FooterComponent,
+    HeaderComponent,
+    RestaComponent,
+    SumaComponent,
+    NavMenuComponent,
+    HomeComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
